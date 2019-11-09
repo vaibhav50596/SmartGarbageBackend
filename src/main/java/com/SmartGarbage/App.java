e6f1aa6c8f2a41ae5f30797f0e5d3e8bdd91eca3
soumoks <soumoks@gmail.com>
2019-11-09 16:49:34 -0700

    Moved App class

--- src/main/java/com/SmartGarbage/App.java
+++ src/main/java/com/SmartGarbage/App.java
@@ -1,11 +1,13 @@
-package com.SmartGarbage.app;
+package com.SmartGarbage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.SmartGarbage"})
 public class App 
 {
     public static void main( String[] args ) {
